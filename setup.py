f3ed28d2e90ff122b73c79ae50fbb4c9c75c4871
Sam Hall <samuel27hall@gmail.com>
2025-05-13 22:49:30 -0500

    Choose unique name for pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="spindle",
+    name="spindle-ml",
     version="0.1.0",
     author="Sam Hall",
     author_email="sam@wafer.systems",
